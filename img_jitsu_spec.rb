002d6046b96c1cd3cd0c99f4b96e4fa0fcbee834
gmalicki@gmail.com <gmalicki@gmail.com>
2008-11-15 19:47:36 -0800

    yep

--- img_jitsu_spec.rb
+++ img_jitsu_spec.rb
@@ -5,9 +5,10 @@ require 'img_jitsu'
 describe Media do  
   it "should be able to make some copies of the downloaded file" do
     @m = Media.new('http://www.google.com/intl/en_ALL/images/logo.gif', 31337)
-    @m.send(:download).should == true
-    @m.send(:resize).should == true
-    @m.send(:sync_to_primary_storage).should == true
-    @m.send(:report).should == true
+    @m.process.should == true
+    # @m.send(:download).should == true
+    # @m.send(:resize).should == true
+    # @m.send(:sync_to_primary_storage).should == true
+    # @m.send(:report).should == true
   end
 end
